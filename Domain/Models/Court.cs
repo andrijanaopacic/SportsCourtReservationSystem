e4df22494e29ceca29357a50c14226e8c61f5dfe
unknown <andrijana.opacic04@gmail.com>
2026-06-12 23:03:26 +0200

    Added domain model Court i Sport

---
+++ Domain/Models/Court.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reservation.Domain.Models
+{
+    public class Court
+    {
+        public int CourtId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal PricePerHour { get; set; }
+        public bool IsIndoor { get; set; }
+
+
+        public int SportId { get; set; }
+        public Sport Sport { get; set; }
+    }
+}
---
+++ Domain/Models/Sport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reservation.Domain.Models
+{
+    public class Sport
+    {
+        public int SportId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int MaxPlayers { get; set; }
+
+
+        public List<Court> Courts { get; set; } = new();
+    }
+}
